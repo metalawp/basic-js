c6600dcd0df646062da67ce8443df2cc0139b069
Andrey Krainov <metal.awp@gmail.com>
2021-03-30 22:13:43 +0300

    feat: extended-repeater and recursive-depth

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,6 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError("Not implemented");
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let addition = options.addition || '';
+  options.addition === false ? addition = 'false' : false;
+  options.addition === null ? addition = 'null' : false;
+  let additionSeparator = options.additionSeparator || '|';
+  let additionRepeatTimes = options.additionRepeatTimes || 1;
+  let separator = options.separator || '+';
+  let repeatTimes = options.repeatTimes || 1;
+  return (str+(addition+additionSeparator).repeat(additionRepeatTimes-1)+addition+separator).repeat(repeatTimes-1)+(str+(addition+additionSeparator).repeat(additionRepeatTimes-1)+addition);
 };
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -2,7 +2,16 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
   calculateDepth(arr) {
-    throw new CustomError("Not implemented");
-    // remove line with error and write your code here
+    return Array.isArray(arr)
+      ? 1 +
+          Math.max(
+            ...arr.map((item) => {
+              if (item.length === 0) {
+                item.push(1);
+              }
+              return this.calculateDepth(item);
+            })
+          )
+      : 0;
   }
 };
