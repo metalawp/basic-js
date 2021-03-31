db7b41080b4fc7089182ea28fc18ee1d980fdacb
Andrey Krainov <metal.awp@gmail.com>
2021-03-31 19:52:17 +0300

    feat: transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -8,14 +8,14 @@ module.exports = function transform(arr) {
   if (arr == null) {
     throw new Error("Error");
   }
-  let result = arr;
+  let result = [...arr];
   for (let i = 0; i < result.length; i++ ) {
-    switch (arr[i]) {
-      case "--discard-next": result[i+1] !== undefined ? result.splice(i, 2, '', '') : result.splice(i, 1, '');//console.log(result);
-      case "--discard-prev": result[i-1] !== undefined && result[i-1] !== '' ? result.splice(i - 1, 2, '', '') : result.splice(i, 1, '');//console.log(result);
-      case "--double-next":  result[i+1] !== undefined ? result.splice(i, 1, result[i + 1]) : result.splice(i, 1, ''); //console.log(result);
-      case "--double-prev":  result[i-1] !== undefined && result[i-1] !== '' ? result.splice(i, 1, result[i - 1]) : result.splice(i, 1, '');// console.log(result);
+    switch (result[i]) {
+      case "--discard-next": result[i+1] !== undefined ? result.splice(i, 2, undefined, undefined) : result.splice(i, 1, undefined);  continue;
+      case "--discard-prev": result[i-1] !== undefined ? result.splice(i - 1, 2, undefined, undefined) : result.splice(i, 1, undefined); continue;
+      case "--double-next":  result[i+1] !== undefined ? result.splice(i, 1, result[i + 1]) : result.splice(i, 1, undefined);  continue;
+      case "--double-prev":  result[i-1] !== undefined ? result.splice(i, 1, result[i - 1]) : result.splice(i, 1, undefined); continue;
     } 
-  };
-  return result.filter(el => el !== ''); 
+  }
+  return result.filter(el => el !== undefined); 
 };
